a880937c37d34508e7f1ad21c3716b4469f880e5
raunaks13 <foleys.raunak@gmail.com>
2018-06-01 01:32:53 +0530

    added 'about us' & fixed footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
+import { RespComponent } from './resp/resp.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    JumbotronComponent,
     PropertiesComponent,
     HeaderComponent,
-    ContactComponent
+    ContactComponent,
+    RespComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
