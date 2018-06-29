008cae6aa4bf6821e7294bd70a6bb6fb97dfba10
Vaibhva <vabs.m.here@gmail.com>
2018-06-29 18:22:34 +0000

    add animations

--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -1,15 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import { flip } from './../../../animations/flip';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [ flip ]
 })
 export class HomeComponent implements OnInit {
 
   constructor() {
   }
-
   ngOnInit() {
   }
 
