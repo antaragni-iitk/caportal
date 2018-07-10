26404ecaa8bb6651f2ea35a707ed01651952e182
tanmayanand44 <anand44tanmay@gmail.com>
2018-07-10 22:06:33 +0530

    some changes

--- src/app/homepage/components/responsibilities/resp.component.ts
+++ src/app/homepage/components/responsibilities/resp.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./resp.component.css']
 })
 export class RespComponent implements OnInit {
+  public hov=false;
 
   titles = [
     'Advertise',
@@ -33,6 +34,10 @@ export class RespComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  hovered(){
+    console.log(this.hov);
+    this.hov=!this.hov;
   }
 
 }
