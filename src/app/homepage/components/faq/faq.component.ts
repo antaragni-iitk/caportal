71a17a0f0eb8de4cd14cff08271f936d8ec0fc60
MohitKS5 <mohitkumarsingh907@gmail.com>
2018-07-13 00:51:14 +0530

    fetch faq

--- src/app/homepage/components/faq/faq.component.ts
+++ src/app/homepage/components/faq/faq.component.ts
@@ -1,5 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UiService} from '@services/ui.service';
+import {ContentService} from '@services/content.service';
+import {map, tap} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-faq',
@@ -8,11 +10,18 @@ import {UiService} from '@services/ui.service';
 })
 export class FaqComponent implements OnInit {
   @ViewChild('faq') faq: ElementRef;
+  data;
+  len: number;
 
-  constructor(private ui: UiService) {
+  constructor(private ui: UiService, private ares: ContentService) {
   }
 
   ngOnInit() {
+    this.data = this.ares.getArray('ca_faq').pipe(
+      tap((val) => this.len = val.data.length),
+      map((res) => Object.keys(res.data).map(val => res.data[val]))
+    )
+    ;
     this.ui.goFaq.subscribe(() => this.faq.nativeElement.scrollIntoView({behavior: 'smooth', block: 'center'}));
   }
 }
