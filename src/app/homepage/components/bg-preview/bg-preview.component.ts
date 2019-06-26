71fb5978f784401251c366faefad63dc4d28134e
rkchaudhary4 <rkc22042000@gmail.com>
2019-06-27 03:22:48 +0530

    Update background images, remove counter from other divs

--- src/app/homepage/components/bg-preview/bg-preview.component.ts
+++ src/app/homepage/components/bg-preview/bg-preview.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BgPreviewComponent implements OnInit {
   @Input('countTrack') countTrack;
   bgs = [
-    '/assets/images/about.jpg',
-    '/assets/images/why.jpg',
-    '/assets/images/about.jpg',
-    '/assets/images/about.jpg',
-    '/assets/images/faq.jpg',
-    '/assets/images/contact.jpg'
+    'https://drive.google.com/uc?export=view&id=1H4YC3e4w_gaSj5Zvit28fIstGwVu06ow',
+    'https://drive.google.com/uc?export=view&id=1Z-3K0qRb-LC1m_x0kZ62Qh3w63e-kG7M',
+    'https://drive.google.com/uc?export=view&id=1qQVSAVmb2jSNEZWZHx_GUQSsucOZUaq1',
+    'https://drive.google.com/uc?export=view&id=1gr3_S5DVSApvgtgPf8cVETWlNYZprTp4',
+    'https://drive.google.com/uc?export=view&id=15Lferh78no6oRosa5aq8lyC9482uMRf-',
+    'https://drive.google.com/uc?export=view&id=1sLfErFsBnZZWQjf4V0PCsTx-DUuNBEvT'
   ]
 
   constructor() {
